1a0cf8c8bdbdedc448772ad3ad94d51dd37d733d
Muhammad Murtaza Hassan <136434291+murtz544@users.noreply.github.com>
2024-12-01 00:11:44 +0500

    Initial commit

---
+++ backend/controller/messageController.js
@@ -0,0 +1,28 @@
+import {catchAsyncErrors} from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import {Message} from "../models/messageSchema.js";
+
+export const sendMessage = catchAsyncErrors(async (req, res, next) => {
+    const { senderName, subject, message } = req.body;
+    if (!senderName || !subject || !message) {
+        return next(new ErrorHandler("Please fill full form", 400));
+    }
+    const data = await Message.create({ senderName, subject, message });
+    res.status(200).json({
+        success: true,
+        message: "Message Sent",
+        data
+    });
+});
+export const sendMgessage = catchAsyncErrors(async (req, res, next) => {
+    const { senderName, subject, message } = req.body;
+    if (!senderName || !subject || !message) {
+        return next(new ErrorHandler("Please fill full form", 400));
+    }
+    const data = await Message.create({ senderName, subject, message });
+    res.status(200).json({
+        success: true,
+        message: "Message Sent",
+        data
+    });
+});
